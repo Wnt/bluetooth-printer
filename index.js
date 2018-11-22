a11d11b27ee21fbf1e66a4ce3f56da4524c1a5dd
Jonni Nakari <jonni@vaadin.com>
2018-11-22 14:21:55 +0100

    Logo print demo.

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 let encoder = new EscPosEncoder();
 
 document.getElementById('connect')
-	.addEventListener('click', () => {
-		BluetoothPrinter.connect()
-			.then(() => {
-				console.log('connected');
-				
-				BluetoothPrinter.addEventListener('disconnected', () => {
-					console.log('disconnected');
-                });
-			}).catch(function(e) {
-                window.alert('Connection failed, please try again!');
-            });
-    });
+.addEventListener('click', () => {
+	BluetoothPrinter.connect()
+	.then(() => {
+		window.alert('Ready to print!');
+		console.log('connected');
+
+		BluetoothPrinter.addEventListener('disconnected', () => {
+			console.log('disconnected');
+		});
+	}).catch(function(e) {
+		window.alert('Connection failed, please try again!');
+	});
+});
 
 document.getElementById('print')
-	.addEventListener('click', () => {
-        if (!BluetoothPrinter.isConnected()) {
-            window.alert('Printer is not connected!');
-            return;
-        }
-        // normal text: 32 characters per line
-        let command = encoder
-            .align('right')
-            .line('This line is aligned to the right')
-            .align('center')
-            .line('This line is centered')
-            .align('left')
-            .line('This line is aligned to the left')
-            .encode()
-		BluetoothPrinter.print(command);
+.addEventListener('click', () => {
+	if (!BluetoothPrinter.isConnected()) {
+		window.alert('Printer is not connected!');
+		return;
+	}
+	// normal text: 32 characters per line
+	// small text: 42 characters per line
+	let command = encoder
+	.text('This is ')
+	.underline()
+	.text('underlined')
+	.underline()
+	.text(' an this is not')
+	.newline()
+	.text('8<------------------------------')
+	.newline()
+	.newline()
+	.newline()
+	.encode()
+	BluetoothPrinter.print(command);
+});
+document.getElementById('print-logo')
+.addEventListener('click', () => {
+	if (!BluetoothPrinter.isConnected()) {
+		window.alert('Printer is not connected!');
+		return;
+	}
+	var img = new Image();
+	img.src = 'vaadin-logo.svg';
+
+	img.onload = function() {
+		BluetoothPrinter.print(encoder
+			.image(img, 384, 88, 'atkinson')
+			.text('384 x 88')
+			.newline()
+			.newline()
+			.newline()
+			.encode());
+		}
+
 	});
